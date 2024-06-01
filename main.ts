820a287d2435d4b92f3f552d0ec5c8b4d0b10a9b
maxi-io <lily.richards@live.com>
2024-06-01 20:49:24 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 * Refer to https://core-electronics.com.au/piicodev-button.html
 */
 
-//% weight=9 color=#666699 icon="\uf001" block="DFPlayer Pro"
+//% weight=9 color=#666699 icon="\uf001" block="PiicoDev Button"
 namespace piicodev {
 
     const DEFAULT_BASE_ADDRESS = 0X42;
@@ -15,19 +15,19 @@ namespace piicodev {
         wasPressed = 0X12,
     }
 
-    //% blockId="piicodev_press" block="is pressed"
+    //% blockId="piicodev_ispressed" block="is pressed"
     //% weight=99 blockGap=20
     export function isPressed(): boolean {
         pins.i2cWriteNumber( DEFAULT_BASE_ADDRESS, registerType.isPressed, NumberFormat.Int8LE, false)
-        let invalue = pins.i2cReadNumber(66, NumberFormat.Int8LE, false)
+        let invalue = pins.i2cReadNumber( DEFAULT_BASE_ADDRESS, NumberFormat.Int8LE, false)
         return (invalue==1)
     }
 
-    //% blockId="piicodev_press" block="was pressed"
+    //% blockId="piicodev_waspressed" block="was pressed"
     //% weight=99 blockGap=20
     export function wasPressed(): boolean {
         pins.i2cWriteNumber( DEFAULT_BASE_ADDRESS, registerType.wasPressed, NumberFormat.Int8LE, false)
-        let invalue = pins.i2cReadNumber(66, NumberFormat.Int8LE, false)
+        let invalue = pins.i2cReadNumber( DEFAULT_BASE_ADDRESS, NumberFormat.Int8LE, false)
         return (invalue == 1)
     }
 
