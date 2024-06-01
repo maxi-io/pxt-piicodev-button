4ca52dc3cbf5c81fbea4a1ab08383d76b86c3597
maxi-io <lily.richards@live.com>
2024-06-01 22:19:05 +1000

    Expand register enum, reversed is pressed logic.

--- main.ts
+++ main.ts
@@ -6,13 +6,38 @@
 //% weight=9 color=#666699 icon="\uf001" block="PiicoDev Button"
 namespace piicodev {
 
-    const DEFAULT_BASE_ADDRESS = 0X42;
+    const DEFAULT_BASE_ADDRESS = 0x42;
 
     export enum registerType {
+        //% block="Who Am I"
+        whoAmI = 0x01,
+        //% block="Firmware Maj"
+        firmwareMaj = 0x02,
+        //% block="Firmware Min"
+        firmwareMin = 0x03,
+        //% block="I2C Address"
+        i2cAddress = 0x04,
+        //% block="LED"
+        led = 0x05,
         //% block="Is Pressed"
-        isPressed = 0X11,
+        isPressed = 0x11,
         //% block="Was Pressed"
-        wasPressed = 0X12,
+        wasPressed = 0x12,
+        //% block="Double Press Detected"
+        doublePressDetected = 0x13,
+        //% block="Press Count"
+        pressCount = 0x14,
+    }
+
+    export enum switchType {
+        //% block="Off"
+        Off = 0,
+        //% block="On"
+        On = 1,
+    }
+
+    function setBit(x: number, n: number): number {
+        return x | (1 << n);
     }
 
     //% blockId="piicodev_ispressed" block="is pressed"
@@ -20,7 +45,7 @@ namespace piicodev {
     export function isPressed(): boolean {
         pins.i2cWriteNumber( DEFAULT_BASE_ADDRESS, registerType.isPressed, NumberFormat.Int8LE, false)
         let invalue = pins.i2cReadNumber( DEFAULT_BASE_ADDRESS, NumberFormat.Int8LE, false)
-        return (invalue==1)
+        return (invalue==0)
     }
 
     //% blockId="piicodev_waspressed" block="was pressed"
@@ -29,6 +54,22 @@ namespace piicodev {
         pins.i2cWriteNumber( DEFAULT_BASE_ADDRESS, registerType.wasPressed, NumberFormat.Int8LE, false)
         let invalue = pins.i2cReadNumber( DEFAULT_BASE_ADDRESS, NumberFormat.Int8LE, false)
         return (invalue == 1)
+    }
+
+    //% blockId="piicodev_led_status" block="is LED on"
+    //% weight=99 blockGap=20
+    export function isLedOn(): boolean {
+        pins.i2cWriteNumber(DEFAULT_BASE_ADDRESS, registerType.led, NumberFormat.Int8LE, false)
+        let invalue = pins.i2cReadNumber(DEFAULT_BASE_ADDRESS, NumberFormat.Int8LE, false)
+        return (invalue == 1)
+    }
+
+    //% blockId="piicodev_led_status" block="Switch LED %status"
+    //% weight=99 blockGap=20
+    export function setLed(status:switchType): void {
+     // TODO
+     // (DEFAULT_BASE_ADDRESS, setBit(registerType.led,7), NumberFormat.Int8LE, false)
+     //   pins.i2cWriteBuffer(DEFAULT_BASE_ADDRESS,, false)
     }
 
 }
